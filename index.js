b5b5b99107fc14034d5feb2134dd1cb4d3718fc9
Dustin Astrin <dustastrin@gmail.com>
2017-11-16 23:23:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  let ranks = document.querySelectorAll('ul.ranked-list li')
+     for (let i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+     }
+}
+
+function deepestChild() {
+  let deepChild = document.getElementById('grand-node').querySelectorAll('div')
+  return deepChild[deepChild.length-1]}
